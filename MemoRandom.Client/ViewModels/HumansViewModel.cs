73d35fb64df657d4230051aef6964fa24e307e62
Вячеслав <kotarius@techpribor.ru>
2022-07-08 09:41:33 +0300

    Now without regions

--- MemoRandom.Client/ViewModels/HumansViewModel.cs
+++ MemoRandom.Client/ViewModels/HumansViewModel.cs
@@ -1,18 +1,9 @@
 ﻿using DryIoc;
-using MemoRandom.Models.Common;
-using Microsoft.Win32;
 using NLog;
 using Prism.Commands;
 using Prism.Events;
 using Prism.Mvvm;
-using Prism.Regions;
 using System;
-using System.Configuration;
-using System.IO;
-using System.Linq;
-using System.Windows;
-using MemoRandom.Client.UserControls;
-using MemoRandom.Models.Events;
 
 namespace MemoRandom.Client.ViewModels
 {
@@ -24,14 +15,8 @@ namespace MemoRandom.Client.ViewModels
         #region PRIVATE FIELDS
         private string _humansViewTitle = "Начало";
         private readonly ILogger _logger; // Экземпляр журнала
-        private readonly IContainer? _container; // Контейнер
-        private readonly IRegionManager _regionManager; // Менеджер регионов
+        private readonly IContainer _container; // Контейнер
         private readonly IEventAggregator _eventAggregator;
-
-        private IRegion _menuRegion;
-        private IRegion _workRegion;
-        private object _menu;
-        private object _work;
         #endregion
 
         #region PROPS
@@ -62,121 +47,6 @@ namespace MemoRandom.Client.ViewModels
         public DelegateCommand AddNewHumanCommand { get; private set; }
         #endregion
 
-        ///// <summary>
-        ///// Метод смены заголовка окна
-        ///// </summary>
-        ///// <param name="message"></param>
-        //private void OnChangeHeader(string message)
-        //{
-        //    if (!string.IsNullOrEmpty(message))
-        //    {
-        //        Title = HeaderDefault + " " + message;
-        //    }
-        //    else
-        //    {
-        //        Title = HeaderDefault;
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Вызов окна списка людей
-        ///// </summary>
-        //private void OnHumansListMenuCommand()
-        //{
-        //    if (_menuRegion.Views.Any())
-        //    {
-        //        _menuRegion.RemoveAll();
-        //        _menu = _container.Resolve<HumansListUserControl>(); // На замену на меню списка людей
-        //        _menuRegion.Add(_menu);
-        //    }
-
-        //    if (_workRegion.Views.Any())
-        //    {
-        //        _workRegion.RemoveAll();
-
-        //        _work = _container.Resolve<HumansView>();
-        //        _workRegion.Add(_work);
-        //    }
-        //}
-
-        /// <summary>
-        /// Вызов окна добавления человека
-        /// </summary>
-        private void OnStartHumansMainView()
-        {
-            if (_menuRegion.Views.Any())
-            {
-                _menuRegion.RemoveAll();
-                _menu = _container.Resolve<HumansMainMenuUserControl>(); // На замену на меню списка людей
-                _menuRegion.Add(_menu);
-            }
-
-            //if (_workRegion.Views.Any())
-            //{
-            //    _workRegion.RemoveAll();
-
-            //    _work = _container.Resolve<HumanInformationView>();
-            //    _workRegion.Add(_work);
-            //    //_workRegion.Context = new HumanInformationViewModel(_eventAggregator);
-            //}
-        }
-
-        ///// <summary>
-        ///// Выбор меню настроек
-        ///// </summary>
-        //private void OpenSettingsView()
-        //{
-        //    if (_menuRegion.Views.Any())
-        //    {
-        //        _menuRegion.RemoveAll();
-        //        _menu = _container.Resolve<SettingsMenuUserControl>();
-        //        _menuRegion.Add(_menu);
-        //    }
-
-        //    if (_workRegion.Views.Any())
-        //    {
-        //        _workRegion.RemoveAll();
-
-        //        _work = _container.Resolve<SettingsView>();
-        //        _workRegion.Add(_work);
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Возврат к стартовому состоянию
-        ///// </summary>
-        //private void OpenStartView()
-        //{
-        //    if (_menuRegion.Views.Any())
-        //    {
-        //        _menuRegion.RemoveAll();
-        //        _menu = _container.Resolve<StartMenuUserControl>();
-        //        _menuRegion.Add(_menu);
-        //    }
-
-        //    if (_workRegion.Views.Any())
-        //    {
-        //        _workRegion.RemoveAll();
-
-        //        _work = _container.Resolve<StartPageUserControl>();
-        //        _workRegion.Add(_work);
-        //    }
-
-        //    var tempevent = _eventAggregator.GetEvent<ChangeViewHeaderEvent>();
-        //    tempevent.Publish(String.Empty);
-        //}
-
-        /// <summary>
-        /// Окно "О программе"
-        /// </summary>
-        private void OpenAboutView()
-        {
-            //TODO Заменить на вызов окна "О программе"
-            //_dialogServices.ShowNotificationDialog("Test!", "This is test message", DialogTypesEnum.DialogNitificationType);
-
-            //MessageBox.Show("Test", "Test1");
-        }
-
         /// <summary>
         /// Инициализация команд
         /// </summary>
@@ -189,35 +59,11 @@ namespace MemoRandom.Client.ViewModels
             //AddNewHumanCommand = new DelegateCommand(OnAddNewHumanCommand);
         }
 
-        /// <summary>
-        /// Инициализация регионов by UC
-        /// </summary>
-        private void InitializeRegions()
-        {
-            _menuRegion = _regionManager.Regions[RegionNames.MenuRegion];
-            if (_menuRegion != null)
-            {
-                _menu = _container.Resolve<HumansMainMenuUserControl>();
-                _menuRegion.Add(_menu);
-            }
-
-            _workRegion = _regionManager.Regions[RegionNames.WorkRegion];
-            if (_menuRegion != null)
-            {
-                _work = _container.Resolve<MainPageUserControl>();
-                _workRegion.Add(_work);
-            }
-
-            var tempevent = _eventAggregator.GetEvent<ChangeViewHeaderEvent>(); // Смена заголовка окна
-            tempevent.Publish(string.Empty);
-        }
-
         #region CTOR
-        public HumansViewModel(ILogger logger, IContainer container, IRegionManager regionManager, IEventAggregator eventAggregator)
+        public HumansViewModel(ILogger logger, IContainer container, IEventAggregator eventAggregator)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _container = container ?? throw new ArgumentNullException(nameof(container));
-            _regionManager = regionManager ?? throw new ArgumentNullException(nameof(regionManager));
             _eventAggregator = eventAggregator ?? throw new ArgumentNullException(nameof(eventAggregator));
 
             //Title = HeaderDefault;
@@ -227,10 +73,9 @@ namespace MemoRandom.Client.ViewModels
             //_reasonsDictionaryChanging = eventAggregator.GetEvent<ChangeReasonsDictionaryEvent>().Subscribe(OnReasonsDictionaryChanged, ThreadOption.PublisherThread);
             //_humansDataFileChanging = eventAggregator.GetEvent<ChangeHumansDataFile>().Subscribe(OnChangeHumansDataFile, ThreadOption.PublisherThread);
 
-            InitializeRegions();
             InitializeCommands();
 
-            OnStartHumansMainView();
+            //OnStartHumansMainView();
         }
         #endregion
     }
--- MemoRandom.Client/Views/HumansView.xaml.cs
+++ MemoRandom.Client/Views/HumansView.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System.Windows;
-using DryIoc;
-using MahApps.Metro.Controls;
+﻿using MahApps.Metro.Controls;
 using MemoRandom.Client.ViewModels;
-using MemoRandom.Models.Common;
-using Prism.Regions;
 
 namespace MemoRandom.Client.Views
 {
@@ -12,31 +8,17 @@ namespace MemoRandom.Client.Views
     /// </summary>
     public partial class HumansView : MetroWindow
     {
-        void SetRegionManager(IRegionManager regionManager, DependencyObject regionTarget, string regionName)
-        {
-            RegionManager.SetRegionName(regionTarget, regionName);
-            RegionManager.SetRegionManager(regionTarget, regionManager);
-        }
-
         private void MetroWindow_ContentRendered(object sender, System.EventArgs e)
         {
             Activate();
         }
 
         #region CTOR
-        public HumansView(IContainer container/*, HumansViewModel vm*/)
+        public HumansView(HumansViewModel vm)
         {
             InitializeComponent();
 
-            var regionManager = container.Resolve<IRegionManager>();
-            if (regionManager != null)
-            {
-                // Установка регионов
-                SetRegionManager(regionManager, WorkRegion, RegionNames.WorkRegion);
-                SetRegionManager(regionManager, MenuRegion, RegionNames.MenuRegion);
-            }
-
-            //DataContext = vm;
+            DataContext = vm;
         }
         #endregion
     }
