72f91e0f0539a2a30c27799c879fd6a374552ce9
Вячеслав <kotarius@techpribor.ru>
2022-08-19 13:40:25 +0300

    Введен класс периода жизни

---
+++ MemoRandom.Models/Models/LifePeriodType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoRandom.Models.Models
+{
+    /// <summary>
+    /// Класс жизненного периода
+    /// </summary>
+    public class LifePeriodType
+    {
+        /// <summary>
+        /// Название периода жизни
+        /// </summary>
+        public string TypeName { get; set; }
+
+        /// <summary>
+        /// Стартовый возраст (в годах), с которого начинается период
+        /// </summary>
+        public int StartAge { get; set; }
+
+        /// <summary>
+        /// Стоповый возраст (в годах), которым заканчивается период
+        /// </summary>
+        public int StopAge { get; set; }
+    }
+}
