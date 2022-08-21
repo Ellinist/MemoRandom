b800ed14ccc76327d5375490ba67d2e7970744f3
ellinist <vgoliney@yandex.ru>
2022-08-21 11:17:37 +0300

    1

--- MemoRandom.Client/Views/HumansListView.xaml.cs
+++ MemoRandom.Client/Views/HumansListView.xaml.cs
@@ -26,6 +26,18 @@ namespace MemoRandom.Client.Views
             this.Loaded += _vm.HumansListView_Loaded; // Событие открытия окна
             this.Closed += _vm.HumansListView_Closed; // Событие закрытия окна
             DgHumans.Sorting += _vm.DgHumans_Sorting; // Событие сортировки по столбцу
+            DgHumans.SelectionChanged += DgHumans_SelectionChanged;
+        }
+
+        /// <summary>
+        /// Метод позиционирования на добавленной или отредактированной записи в DataGrid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DgHumans_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            DgHumans.UpdateLayout();
+            DgHumans.ScrollIntoView(DgHumans.SelectedItem, null);
         }
         #endregion
     }
