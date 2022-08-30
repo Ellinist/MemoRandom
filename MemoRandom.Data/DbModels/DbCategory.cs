5cc9cfbb982019154021f294537513467a49780d
ellinist <vgoliney@yandex.ru>
2022-08-30 19:20:46 +0300

    Comments

--- MemoRandom.Data/DbModels/DbCategory.cs
+++ MemoRandom.Data/DbModels/DbCategory.cs
@@ -1,30 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MemoRandom.Data.DbModels
 {
+    /// <summary>
+    /// Класс категории человеческого возраста
+    /// </summary>
     public class DbCategory
     {
+        /// <summary>
+        /// Идентификатор категории
+        /// </summary>
         [Key]
         [Required]
         public Guid DbCategoryId { get; set; }
 
+        /// <summary>
+        /// Название категории
+        /// </summary>
         [Required]
         public string DbCategoryName { get; set; }
 
+        /// <summary>
+        /// С какого возраста действует категория
+        /// </summary>
         [Required]
         public int DbPeriodFrom { get; set; }
 
+        /// <summary>
+        /// До какого возраста действует категория
+        /// </summary>
         [Required]
         public int DbPeriodTo { get; set; }
 
+        /// <summary>
+        /// Яркость цвета
+        /// </summary>
+        [Required]
         public byte DbColorA { get; set; }
+
+        /// <summary>
+        /// Красный цвет
+        /// </summary>
+        [Required]
         public byte DbColorR { get; set; }
+
+        /// <summary>
+        /// Зеленый цвет
+        /// </summary>
+        [Required]
         public byte DbColorG { get; set; }
+
+        /// <summary>
+        /// Синий цвет
+        /// </summary>
+        [Required]
         public byte DbColorB { get; set; }
     }
 }
