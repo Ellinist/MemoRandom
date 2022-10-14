799acdbd6db90e978f15859a755ce6c318cb7bf6
Вячеслав <kotarius@techpribor.ru>
2022-10-14 11:17:34 +0300

    Подготовлена заготовка с командами по людям для сравнения

--- MemoRandom.Client/ViewModels/ComparedHumansViewModel.cs
+++ MemoRandom.Client/ViewModels/ComparedHumansViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using MemoRandom.Data.Interfaces;
+using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,8 @@ namespace MemoRandom.Client.ViewModels
     public class ComparedHumansViewModel :BindableBase
     {
         #region PRIVATE FIELDS
+        private readonly IMsSqlController _msSqlController;
+
         private string _comparedHumansTitle = "Люди для сравнения";
         #endregion
 
@@ -32,10 +36,43 @@ namespace MemoRandom.Client.ViewModels
         #endregion
 
         #region COMMANDS
+        /// <summary>
+        /// Команда добавления человека для сравнения
+        /// </summary>
+        public DelegateCommand NewComparedHumanCommand { get; private set; }
 
+        /// <summary>
+        /// Команда сохранения человека для сравнения во внешнем хранилище
+        /// </summary>
+        public DelegateCommand SaveComparedHumanCommand { get; private set; }
+
+        /// <summary>
+        /// Команда удаления выбранного человека для сравнения
+        /// </summary>
+        public DelegateCommand DeleteComparedHumanCommand { get; private set; }
         #endregion
 
+        private void NewComparedHuman()
+        {
 
+        }
+
+        private void SaveComparedHuman()
+        {
+
+        }
+
+        private void DeleteComparedHuman()
+        {
+
+        }
+
+        private void InitCommands()
+        {
+            NewComparedHumanCommand = new DelegateCommand(NewComparedHuman);
+            SaveComparedHumanCommand = new DelegateCommand(SaveComparedHuman);
+            DeleteComparedHumanCommand = new DelegateCommand(DeleteComparedHuman);
+        }
 
 
 
@@ -46,9 +83,11 @@ namespace MemoRandom.Client.ViewModels
         /// <summary>
         /// Конструктор
         /// </summary>
-        public ComparedHumansViewModel()
+        public ComparedHumansViewModel(IMsSqlController msSqlController)
         {
+            _msSqlController = msSqlController ?? throw new ArgumentNullException(nameof(msSqlController));
 
+            InitCommands();
         }
         #endregion
     }
