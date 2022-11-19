ac16c2d5fc2cb5f7b16873683016955b00354051
Вячеслав <kotarius@techpribor.ru>
2022-11-19 13:32:32 +0300

    That's all for now

---
+++ MemoRandom.Client/ViewModels/ComparingProcessViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoRandom.Client.ViewModels
+{
+    /// <summary>
+    /// Модель представления для хранения UC с прогрессом
+    /// </summary>
+    public class ComparingProcessViewModel
+    {
+
+    }
+}
