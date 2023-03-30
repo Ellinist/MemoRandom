183bd3feb94847793cbfefffd6bd2e95eb9aafe7
Вячеслав <kotarius@techpribor.ru>
2023-03-30 09:41:08 +0300

    Testing deleted

--- MemoRandom.Tests/UnitTest1.cs
+++ MemoRandom.Tests/UnitTest1.cs
@@ -1,11 +1,31 @@
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace MemoRandom.Tests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void MemoRandomTest()
+        public void MemoRandom_ReadingReasonsTest()
         {
+            // Arrange
+            //ObservableCollection<Reason> 
+
+            // Act
+
+            //Assert
+
+        }
+
+        [TestMethod]
+        public void MemoRandom_ReadingHumansTest()
+        {
+            // Arrange
+
+            // Act
+
+            //Assert
 
         }
     }
